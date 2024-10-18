cd8db62ad4432d2cd4d32bef9470f0e03dea39d1
kurth4cker <kurth4cker@gmail.com>
2024-10-18 10:32:02 +0300

    sample.py: change license

--- sample.py
+++ sample.py
@@ -1,8 +1,18 @@
-# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-License-Identifier: ISC
 # SPDX-FileCopyrightText: 2024 kurth4cker <kurth4cker@gmail.com>
 
-def say_hello(to = 'world'):
-    print('hello', to)
+num = int(input("Bir sayı giriniz: "))
+if num < 1:
+    raise ValueError(f'{num} pozitif değil')
 
-if __name__ == '__main__':
-    say_hello()
+divider = 2
+dividers = set()
+
+while num != 1:
+    if num % divider == 0:
+        dividers.add(divider)
+        num //= divider
+    else:
+        divider += 1
+
+print(f'{num} sayısının tam bölenleri:', *dividers)
