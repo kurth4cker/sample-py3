f508236169769ada75cf5be550aa357104c8ef44
kurth4cker <kurth4cker@gmail.com>
2024-12-30 19:49:48 +0300

    sample/__main__.py: hello world

--- sample/__main__.py
+++ sample/__main__.py
@@ -1,18 +1,4 @@
 # SPDX-License-Identifier: ISC
 # SPDX-FileCopyrightText: 2024 kurth4cker <kurth4cker@gmail.com>
 
-num = int(input("Bir sayı giriniz: "))
-if num < 1:
-    raise ValueError(f'{num} pozitif değil')
-
-divider = 2
-dividers = set()
-
-while num != 1:
-    if num % divider == 0:
-        dividers.add(divider)
-        num //= divider
-    else:
-        divider += 1
-
-print(f'{num} sayısının tam bölenleri:', *dividers)
+print("hello world")
